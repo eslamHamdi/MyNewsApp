fbe3a814353278c706af53b2e3beacfbef3fbf70
eslamHamdi <63453947+eslamHamdi@users.noreply.github.com>
2021-04-14 21:58:05 +0200

    starter

---
+++ app/src/main/java/com/example/mynews/MyNewsApp.kt
@@ -0,0 +1,10 @@
+package com.example.mynews
+
+import android.app.Application
+
+class MyNewsApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/com/example/mynews/ui/NewsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mynews.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mynews.R
+
+class NewsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news)
+    }
+}
